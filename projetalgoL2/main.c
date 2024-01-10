6cc1bef7c0e5d2ba95ba5ddf0f0c1ee6109a0a19
lydiamendjour <155400932+lydiamendjour@users.noreply.github.com>
2024-01-10 03:59:26 +0100

    création de la liste

--- projetalgoL2/main.c
+++ projetalgoL2/main.c
@@ -1,1 +1,37 @@
 //my code starts here
+#include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+} Node;
+
+Node* head = NULL;
+Node* deletedNodes = NULL;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertNode(int data) {
+    Node* newNode = createNode(data);
+
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        Node* current = head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+        newNode->prev = current;
+    }
+}
